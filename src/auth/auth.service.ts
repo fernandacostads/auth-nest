1180fccfd9179e5f71a705512f979e1dda8341ca
Fernanda Costa <fernandacosta@alu.ufc.br>
2023-12-26 17:36:00 -0300

    add JWT access and refresh tokens

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,14 +2,38 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from './dto/auth.dto';
 import { UserService } from 'src/user/user.service';
 import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService,
+    private jwtService:JwtService
+    ) {}
 
   async login(dto: LoginDto) {
 
     const user = await this.validateUser(dto);
+    const payload = {
+      username:user.email,
+      sub: {
+        name: user.name,
+
+      },
+    };
+
+    return {
+      user,
+      backendTokens:{
+        acessToken: await this.jwtService.signAsync(payload, {
+          expiresIn: '1h',
+          secret: process.env.jwtSecretKey,
+        }),
+        RefreshToken: await this.jwtService.signAsync(payload, {
+            expiresIn: '7d',
+            secret: process.env.jwtRefreshTokenKey,
+          }),
+      }
+    }
   }
 
   async validateUser(dto: LoginDto) {
